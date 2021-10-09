06057a722f6c62f92d9203ef1159c77066f9b5fc
woo94 <bestman21c@naver.com>
2021-10-09 15:20:01 +0900

    file upload process is handled in global scope

---
+++ src/util/appState/fileUploadSlice.ts
@@ -0,0 +1,56 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import {ref as storageRef, uploadBytes, getDownloadURL} from 'firebase/storage'
+import {RootState} from './Store'
+import {v4 as uuidv4} from 'uuid'
+import {getFunctions, httpsCallable} from 'firebase/functions'
+
+interface FileUploadState {
+    // key: uuid
+    // value: state -> uploading / success / fail
+    [key: string]: string;
+}
+
+
+const initialState: FileUploadState = {
+
+}
+
+export const uploadStudyGroupFileMessage = createAsyncThunk(
+    'fileUpload/uploadStudyGroupFileMessage',
+    async (data: { sender: string; fileName: string, fileBlob: Blob, filePath: ReturnType<typeof storageRef>; }) => {
+        const metadata = {
+            customMetadata: {
+                sender: data.sender,
+                filename: data.fileName
+            },
+            cacheControl: 'private, max-age=15552000'
+        }
+
+        return uploadBytes(data.filePath, data.fileBlob, metadata)
+    }
+)
+
+export const fileUploadSlice = createSlice({
+    name: 'fileUpload',
+    initialState,
+    reducers: {
+
+    },
+    extraReducers: (builder) => {
+        builder.addCase(uploadStudyGroupFileMessage.pending, (state, action) => {
+            state[action.meta.arg.fileName] = 'uploading'
+        })
+
+        builder.addCase(uploadStudyGroupFileMessage.fulfilled, (state, action) => {
+            state[action.meta.arg.fileName] = 'success'
+        })
+
+        builder.addCase(uploadStudyGroupFileMessage.rejected, (state, action) => {
+            state[action.meta.arg.fileName] = "fail"
+        })
+    }
+})
+
+export const selectFileUpload = (state: RootState) => state.fileUpload
+
+export default fileUploadSlice.reducer
