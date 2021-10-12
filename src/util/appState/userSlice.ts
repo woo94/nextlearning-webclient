d1630cba9bd859c36ccd9eb2148cf86dd51ee1d7
woo94 <bestman21c@naver.com>
2021-10-12 16:34:08 +0900

    user doucment is read outside the reducers because its data is used in another slices either

--- src/util/appState/userSlice.ts
+++ src/util/appState/userSlice.ts
@@ -13,7 +13,6 @@ export interface Friend extends PublicInfo {
 export interface UserState extends PublicInfo {
     idToken: string;
     isLogin: boolean;
-    online: boolean;
 }
 
 const initialState: UserState = {
@@ -23,7 +22,6 @@ const initialState: UserState = {
     uid: '',
     isLogin: false,
     name: '',
-    online: false
 }
 
 export const submitLoginInfo = createAsyncThunk(
@@ -33,39 +31,6 @@ export const submitLoginInfo = createAsyncThunk(
         return userCredential.user?.uid
     }
 )
-
-// due to creation of friendSlice, read user doc and dispatch to both slice synchronously
-// export const getUserDoc = createAsyncThunk(
-//     'user/getUserDoc',
-//     async (uid: string) => {
-//         const firestore = firebase.firestore()
-//         const userDocRef = firestore.collection("user").doc(uid)
-        
-//         const userDoc = await userDocRef.get()
-
-//         const userDocData = <firebase.firestore.DocumentData>userDoc.data()
-        
-//         const friendRequestDocRef = firestore.collection("user").doc(uid).collection("public").doc("friend_request")
-//         const friendRequestDoc = await friendRequestDocRef.get()
-//         const friendRequestDocData = <firebase.firestore.DocumentData>friendRequestDoc.data()
-//         console.log(friendRequestDocData)
-
-//         const candidateInfoDocRefs: Array<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>> = [...friendRequestDocData["sent"], ...friendRequestDocData["received"]].map((user: {uid: string, tp: number}) => firestore.collection("user").doc(user.uid).collection("public").doc("info"))
-//         console.log(candidateInfoDocRefs)
-//         const candidateInfoDocsData = await Promise.all(candidateInfoDocRefs.map(async (ref) => ref.get().then(doc => doc.data())))
-//         console.log(candidateInfoDocsData)
-
-//         const friendsInfoDocRefs: Array<firebase.firestore.DocumentReference<firebase.firestore.DocumentData>> = userDocData['friend_list'].map((uid: string) => firestore.collection("user").doc(uid).collection("public").doc("info"))
-//         const friendsInfoDocsData = await Promise.all(friendsInfoDocRefs.map(async (ref) => ref.get().then(doc => doc.data())))
-
-//         return {
-//             userData: userDocData,
-//             friendsInfoData: friendsInfoDocsData,
-//             friendRequestDocData,
-//             candidateInfoDocsData
-//         }
-//     }
-// )
 
 export const requestIdToken = createAsyncThunk(
     'user/getIdToken',
@@ -98,8 +63,10 @@ export const userSlice = createSlice({
         setIsLogin: (state, action) => {
             state.isLogin = action.payload
         },
-        setIsOnline: (state, action) => {
-            state.online = action.payload
+        setMyinfo: (state, action) => {
+            state.email = action.payload.email
+            state.img = action.payload.img
+            state.name = action.payload.name
         }
     },
 
@@ -118,12 +85,11 @@ export const userSlice = createSlice({
             state.isLogin = false
             state.uid = ''
             state.name = ""
-            state.online = false
         })
     }
 })
 
-export const {setUid, setIdToken, setIsLogin, setIsOnline} = userSlice.actions
+export const { setUid, setIdToken, setIsLogin, setMyinfo } = userSlice.actions
 
 export const selectUser = (state: RootState) => state.user
 
