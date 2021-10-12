c90300d9c6644d20ae544c0074ec73c131909eb2
woo94 <bestman21c@naver.com>
2021-10-12 16:33:31 +0900

    change friend info read logic

--- src/util/appState/friendSlice.ts
+++ src/util/appState/friendSlice.ts
@@ -1,10 +1,14 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
 import { RootState } from './Store'
 import { PublicInfo } from './commonTypes'
+import {getDoc, getFirestore, doc} from 'firebase/firestore'
+
+const firestore = getFirestore()
 
 export interface FriendState {
     initFriendList: boolean;
     friend_list: Array<PublicInfo & { online: boolean }>;
+    
     initFriendRequest: boolean;
     friend_request: {
         sent: Array<PublicInfo & {tp: number}>;
@@ -15,6 +19,7 @@ export interface FriendState {
 const initialState: FriendState = {
     initFriendList: false,
     friend_list: [],
+
     initFriendRequest: false,
     friend_request: {
         sent: [],
@@ -22,18 +27,29 @@ const initialState: FriendState = {
     }
 }
 
+export const setFriendList = createAsyncThunk(
+    'friend/setFriendList',
+    async (friendList: Array<string>) => {
+        const friendsInfoPromise = friendList.map(async uid => {
+            const infoDoc = await getDoc(doc(firestore, 'user', uid, 'public', 'info'))
+            return infoDoc.data() as PublicInfo
+        })
+
+        return Promise.all(friendsInfoPromise)
+    }
+)
+
+export const setFriendRequest = createAsyncThunk(
+    'friend/setFriendRequest',
+    async () => {
+        
+    }
+)
+
 export const friendSlice = createSlice({
     name: "friend",
     initialState,
     reducers: {
-        setFriendList: (state, action) => {
-            state.friend_list = action.payload
-            state.initFriendList = true
-        },
-        setFriendRequest: (state, action) => {
-            state.friend_request = action.payload
-            state.initFriendList = true
-        },
         friendOnline: (state, action) => {
             const uid = action.payload
             const idx = state.friend_list.findIndex(friend => friend.uid === uid)
@@ -48,10 +64,21 @@ export const friendSlice = createSlice({
                 state.friend_list[idx].online = false
             }
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(setFriendList.fulfilled, (state, action) => {
+            state.friend_list = action.payload.map(info => {
+                return {
+                    ...info,
+                    online: false
+                }
+            })
+            state.initFriendList = true
+        })
     }
 })
 
-export const {setFriendList, setFriendRequest, friendOnline, friendOffline} = friendSlice.actions
+export const { friendOnline, friendOffline} = friendSlice.actions
 
 export const selectFriend = (state: RootState) => state.friend
 
