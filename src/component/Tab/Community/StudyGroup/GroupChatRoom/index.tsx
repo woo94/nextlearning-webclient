d750451a04828c6c0a2f497bf1e5ce2f9d299215
woo94 <bestman21c@naver.com>
2021-10-12 16:37:47 +0900

    file upload is handled in global scope. chat messages are not stored in global scope but a react component's state

--- src/component/Tab/Community/StudyGroup/GroupChatRoom/index.tsx
+++ src/component/Tab/Community/StudyGroup/GroupChatRoom/index.tsx
@@ -3,16 +3,22 @@ import {useRouteMatch, useHistory, useLocation} from 'react-router-dom'
 import {getDatabase, ref as dbRef, set, get, push, query, limitToLast, onChildAdded, off, startAfter} from 'firebase/database'
 import {getStorage, ref as storageRef, uploadBytes} from 'firebase/storage'
 import TopController from 'src/component/Common/TopController'
-import {selectStudyGroup, pushChat, chatFetched} from 'src/util/appState/studyGroupSlice'
+import {selectStudyGroup} from 'src/util/appState/studyGroupSlice'
 import {selectUser} from 'src/util/appState/userSlice'
 import {useAppSelector, useAppDispatch} from 'src/util/appState/hooks'
 import {TextField, IconButton, Grid, Button, Paper} from '@mui/material'
 import {AttachFile, Image} from '@mui/icons-material'
 import {cyan} from '@mui/material/colors'
 import { v4 as uuidv4 } from 'uuid'
-import {upload, selectFileUpload} from 'src/util/appState/fileUploadSlice'
+import {uploadStudyGroupFileMessage, selectFileUpload} from 'src/util/appState/fileUploadSlice'
+import {getApp} from 'firebase/app'
+import ImageDialog from './ImageDialog'
+import {StudyGroup} from 'src/util/types'
+import produce from 'immer'
 
+const app = getApp()
 const db = getDatabase()
+const storage = getStorage(app, 'gs://nextlearning-505ce-study-group')
 
 function GroupChatRoom() {
     const user = useAppSelector(selectUser)
@@ -20,7 +26,12 @@ function GroupChatRoom() {
     const dispatch = useAppDispatch()
     const studyGroup = useAppSelector(selectStudyGroup)
     const [message, setMessage] = useState('')
-    const [uploadingFiles, setUploadingFiles] = useState<Map<string, string>>(new Map())
+
+    interface CHAT extends StudyGroup.CHAT_MESSAGE {
+        randomKey: string;
+    }
+
+    const [chats, setChats] = useState<Array<CHAT>>([])
     
     const chatViewRef = useRef<null | HTMLDivElement>(null)
     const inputRef = useRef<null | HTMLInputElement>(null)
@@ -29,7 +40,6 @@ function GroupChatRoom() {
     const gid = location.pathname.split('/').pop() || ""
 
     const group = studyGroup.groups.find(val => val.gid === gid)
-    const chatMessages = studyGroup.chats[gid]
 
     const chatRef = dbRef(db, gid)
 
@@ -59,73 +69,67 @@ function GroupChatRoom() {
 
         if(inputRef.current.files.item(0) === null) { return }
 
-        const storage = getStorage()
-        const fileName = uuidv4()
+        const fileName = Math.floor(Date.now() / 1000).toString() + '_' + uuidv4()
 
-        const cloudStorageRef = storageRef(storage, `study_group/${gid}/${fileName}`)
-        dispatch(upload({
+        const cloudStorageRef = storageRef(storage, `${gid}/files/${fileName}`)
+        dispatch(uploadStudyGroupFileMessage({
+            sender: user.uid,
             fileName,
             fileBlob: inputRef.current.files.item(0) as Blob,
             filePath: cloudStorageRef
         }))
 
     }
-
-    useEffect(() => {
-
-    }, [uploadingFiles])
-
+    
     useEffect(() => {
         if(chatViewRef.current) {
             chatViewRef.current.scrollTop = chatViewRef.current.scrollHeight
+            // chatViewRef.current.onscroll = (e) => {
+            //     if(chatViewRef.current?.scrollTop === 0) {
+            //         console.log('show me more message')
+            //     }
+            // }
         }
-    }, [chatMessages.length])
+    }, [chats.length])
 
     useEffect(() => {
-        if(!group?.chatFetched) {
-            const recentChatQuery = query(chatRef, limitToLast(10))
-            get(recentChatQuery).then(messages => {
-                messages.forEach(message => {
-                    dispatch(pushChat({
-                        gid,
-                        data: {
-                            randomKey: message.key,
-                            ...message.val()
-                        }
-                    }))
-                })
-
-                dispatch(chatFetched(gid))
-
-                return Object.keys(messages.val() || {})
+        
+        const recentChatQuery = query(chatRef, limitToLast(10))
+        get(recentChatQuery).then(messages => {            
+            messages.forEach(message => {
+                setChats(
+                    produce((draft) => {
+                        draft.push({...message.val(), randomKey: message.key})
+                    })
+                )
             })
+
+            return Object.keys(messages.val() || {}) 
+        })
             .then((randomKeys) => {
-                if(randomKeys.length === 0) {
+                console.log(randomKeys)
+                if (randomKeys.length === 0) {
                     onChildAdded(query(chatRef), child => {
-                        dispatch(pushChat({
-                            gid,
-                            data: {
-                                randomKey: child.key,
-                                ...child.val()
-                            }
-                        }))
+                        setChats(
+                            produce(draft => {
+                                draft.push({...child.val(), randomKey: child.key})
+                            })
+                        )
                     })
                 }
                 else {
-                    onChildAdded(query(chatRef, startAfter(null, randomKeys[randomKeys.length-1])), child => {
-                        dispatch(pushChat({
-                            gid,
-                            data: {
-                                randomKey: child.key,
-                                ...child.val()
-                            }
-                        }))
+                    onChildAdded(query(chatRef, startAfter(null, randomKeys[randomKeys.length - 1])), child => {
+                        setChats(
+                            produce(draft => {
+                                draft.push({...child.val(), randomKey: child.key})
+                            })
+                        )
                     })
                 }
             })
-        }
 
         return () => {
+            console.log('off listener')
             off(chatRef, "child_added")
         }
     }, [])
@@ -133,21 +137,30 @@ function GroupChatRoom() {
     return (
         <>
             <TopController text={group?.title || ""} actions={[]} />
-            <Grid ref={chatViewRef} overflow="scroll" wrap="nowrap" sx={{height: '80vh'}} direction="column" container>
+            <Grid ref={chatViewRef} wrap="nowrap" sx={{ overflowY: "scroll", height: '80vh'}} direction="column" container>
                 <Grid flexGrow={1} flexShrink={1} item>
                     <Grid direction="column" container>
                     {
-                        chatMessages.map(chat => (
-                            <Grid item>
-                                <Grid flexDirection={chat.sender === user.uid ? "row-reverse" : "row"} container>
-                                    <Grid item>
-                                        <Paper sx={{p: 1, m: 1, bgcolor: cyan[50] }} >
-                                            {chat.message}
-                                        </Paper>
+                        chats.map(chat => {
+                            if(chat.type === "text") {
+                                return (
+                                    <Grid key={chat.randomKey} item>
+                                        <Grid flexDirection={chat.sender === user.uid ? "row-reverse" : "row"} container>
+                                            <Grid item>
+                                                <Paper sx={{ p: 1, m: 1, bgcolor: cyan[50] }} >
+                                                    {chat.message}
+                                                </Paper>
+                                            </Grid>
+                                        </Grid>
                                     </Grid>
-                                </Grid>
-                            </Grid>
-                        ))
+                                )
+                            }
+                            else if(chat.type.startsWith("image")) {
+                                return (
+                                    <ImageDialog myUid={user.uid} sender={chat.sender} downloadURL={chat.message} />
+                                )
+                            }
+                        })
                     }
                     </Grid>
                 </Grid>
@@ -157,7 +170,6 @@ function GroupChatRoom() {
                     onChange={(e) => {setMessage(e.target.value)}} 
                     onKeyPress={(e) => { 
                         if(e.key === 'Enter') {
-                            console.log('enter and send')
                             sendMessage()
                         }
                     }}
