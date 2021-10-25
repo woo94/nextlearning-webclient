44597c014d68f68485f0cf327f5732959f4eab97
woo94 <bestman21c@naver.com>
2021-10-26 07:09:22 +0900

    Add entry point to Monthly Plan in Home tab

--- src/component/Tab/Home/TaskNav.tsx
+++ src/component/Tab/Home/TaskNav.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { grey } from '@material-ui/core/colors'
-import {Box, Grid, Chip} from '@mui/material'
+import {Box, Grid, Chip, IconButton} from '@mui/material'
+import {ArrowForwardIos} from '@mui/icons-material'
 import {Link, useRouteMatch, Switch, Route, RouteComponentProps} from 'react-router-dom'
 
 interface NavItemProps {
@@ -14,7 +15,7 @@ function NavItem(props: NavItemProps) {
     const currentTab = paths[paths.length - 1]
 
     return (
-        <Grid sx={{ py: 1 }} flexGrow={1} item>
+        <Grid sx={{ py: 1 }} flexGrow={2} item>
             <Link style={{textDecoration: "none"}} to={props.path}>
                 <Box sx={{boxShadow: currentTab === props.tabInfo ? 2 : 0 }} color="black" borderRadius="30px" minWidth='100px' px={2} py={1.5} display="inline-block" bgcolor={currentTab === props.tabInfo ? 'white' : 'transparent'}>
                     {props.text}
@@ -31,10 +32,17 @@ export default function TaskNav() {
     
     return (
         <>
-            <Grid my={2} borderRadius="40px" bgcolor={grey[200]} fontSize="1rem" textAlign="center" container>
+            <Grid alignItems="center" my={2} borderRadius="40px" bgcolor={grey[200]} fontSize="1rem" textAlign="center" container>
                 <NavItem tabInfo={currentTab} path="/home/yesterday" text="Yesterday" />
                 <NavItem tabInfo={currentTab} path="/home/today" text="Today" />
-                <NavItem tabInfo={currentTab} path="/home/tommorow" text="Tommorow" />
+                <NavItem tabInfo={currentTab} path="/home/tomorrow" text="Tommorow" />
+                <Grid flexGrow={1} item>
+                    <Link to="/monthly-plan/this_month">
+                        <IconButton>
+                            <ArrowForwardIos />
+                        </IconButton>
+                    </Link>
+                </Grid>
             </Grid>
         </>
     )
