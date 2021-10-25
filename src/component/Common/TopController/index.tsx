f8de82c439cbd4a9297f917cf2997fe9455c87bb
woo94 <bestman21c@naver.com>
2021-10-26 07:03:49 +0900

    MultiActionController is not using further

--- src/component/Common/TopController/index.tsx
+++ src/component/Common/TopController/index.tsx
@@ -1,24 +1,14 @@
 import React, {useContext} from 'react'
 import SingleActionController from "./SingleActionController";
-import MultiActionController from "./MultiActionController"
 import {Props} from './util'
 import Box from '@mui/material/Box'
 
 function TopController(props: Props) {
-    if(props.actions.length <= 1) {
-        return (
-            <Box pt={1} pb={2} >
-                <SingleActionController {...props} />
-            </Box>
-        )
-    }
-    else {
-        return (
-            <Box pt={1} pb={2} >
-                <MultiActionController {...props} />
-            </Box>
-        )
-    }
+    return (
+        <Box pt={1} pb={2} >
+            <SingleActionController {...props} />
+        </Box>
+    )
 }
 
 export default TopController
