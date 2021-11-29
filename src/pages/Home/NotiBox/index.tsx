acf70ea50dde7c65034d13ca1ec2c3dc1c3c407f
woo94 <bestman21c@naver.com>
2021-11-29 13:14:09 +0900

    create NotiBox component

---
+++ src/pages/Home/NotiBox/index.tsx
@@ -0,0 +1,9 @@
+import {Box} from '@mui/material'
+
+export default function NotiBox() {
+    return (
+        <Box sx={{ textAlign: 'center', p: 2, bgcolor: '#fcfcfc', color: '#373737', borderRadius: '1.5rem', my: 2 }}>
+            Notification here Notification here Notification here Notification here Notification here Notification here
+        </Box>
+    )
+}
